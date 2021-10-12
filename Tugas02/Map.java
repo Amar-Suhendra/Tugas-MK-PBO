c8f3dd283d6365f9276fe5f006ae1b82c25dfe16
Suhendra <amarsuhendra2@gmail.com>
2021-10-12 11:59:39 +0700

    version 12.10.21.Alpha-01
    + Added map class
       - InsertMap method
       - PrintMap method
    + added main class
       - main method

---
+++ Tugas02/Map.java
@@ -0,0 +1,55 @@
+
+/**
+ * Using to print the Map for the game 
+ *
+ * @author Amar Suhendra
+ * @version 12.10.21.Alpha-01
+ */
+public class Map
+{
+    // instance variables - replace the example below with your own
+    private int[][] map;
+    private int angka;
+
+    /**
+     * Constructor for objects of class Map
+     */
+    public Map()
+    {
+        // initialise instance variables
+        map = new int[10][10];
+        angka = 1;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    
+    public void InsertMap(){
+        // insert number into array
+        for (int i = 0; i < map.length; i++) {
+          for (int j = 0; j < map.length; j++) {
+            map[i][j] = angka;
+            angka++;
+          }
+        }
+    }
+    
+    public void PrintMap(){
+        // this code gonna print the array as a table
+        System.out.println("=====================================================");
+        for (int i = 0; i < map.length; i++) {
+          for (int j = 0; j < map.length; j++) {
+            System.out.printf("%5d", map[i][j]);
+             if (j == 9) {
+             System.out.print("\n");
+            }
+          }
+        }
+       System.out.println("=====================================================");
+    }
+    
+}
---
+++ Tugas02/main.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class main here.
+ *
+ * @author Amar Suhendra
+ * @version 12.10.21.Alpha-01
+ */
+public class main
+{
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public static void main(String[] args){
+        Map map1 = new Map();
+        
+        map1.InsertMap();
+        map1.PrintMap();
+    }
+}
